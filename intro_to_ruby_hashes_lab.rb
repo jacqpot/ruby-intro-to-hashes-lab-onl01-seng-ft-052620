33ef3c8a6336c509ee88e2bbb078ff46c239e3d1
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-01 17:29:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
 	      },
 	      :b_and_o_railroad => 
 	      {
-	        "mortgage_value" => ""
+	        "mortgage_value" => "$400"
 	      },
 	      :shortline => 
 	      {
